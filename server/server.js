867733acff05fd02e3c0b443721049f79637f1e7
Manishgupta3239 <160010708+Manishgupta3239@users.noreply.github.com>
2026-01-11 15:19:14 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-  origin: 'https://cryptotracker-aee1b.web.app',
+  origin: 'http://localhost:5173',
   methods: ['GET', 'POST'],
   credentials: true,
 }));
