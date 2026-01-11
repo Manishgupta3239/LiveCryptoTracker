86ddf58f52c089c11c4bcfc5366efdddb8a79a5c
Manish Gupta <guptamanish3239@gmail.com>
2026-01-11 19:49:41 +0530

    dockerized app

--- server/server.js
+++ server/server.js
@@ -6,13 +6,12 @@ import { connectDb } from "./config/db.js";
 import axios from "axios";
 import cron from "node-cron";
 
-
 dotenv.config();
 
 const app = express();
 
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: ['https://cryptotracker-aee1b.web.app' , 'http://localhost:5173/' ],
   methods: ['GET', 'POST'],
   credentials: true,
 }));
@@ -22,7 +21,7 @@ app.use(express.json());
 app.use("/api" , coinsRoute);
 // cron job run after 1hr
 cron.schedule('0 * * * *', async () => {
-
+   await axios.post("https://cryptotracker-aee1b.web.app/api/history");
    console.log("data saved");
 });
 
@@ -31,4 +30,3 @@ connectDb();
 app.listen(process.env.PORT, ()=>{
     console.log(`listing to port ${process.env.PORT}`);
 })
-
